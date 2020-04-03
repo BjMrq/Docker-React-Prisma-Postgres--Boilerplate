4a19776c04d8b7cd13a9aecaaaf17dce2756b15b
Benjamin Marquis <bmarquiscom@gmail.com>
2020-04-03 09:15:48 -0400

    Update Dockerfile

--- app/src/config.js
+++ app/src/config.js
@@ -10,7 +10,6 @@ export const DB_URL          = process.env.MONGO_URL;
 export const DB_PORT         = process.env.MONGO_PORT;
 export const DB_USER         = process.env.DB_USER;
 export const DB_PASSWORD     = process.env.DB_PASSWORD;
-export const PRISMA_ENDPOINT = process.env.NODE_ENV ? 
-  `${process.env.PRISMA_ENDPOINT}/test/test` : process.env.PRISMA_ENDPOINT;
+export const PRISMA_ENDPOINT = process.env.PRISMA_ENDPOINT;
 export const PRISMA_SECRET   = process.env.PRISMA_SECRET;
 export const JWT_SECRET      = process.env.JWT_SECRET;
